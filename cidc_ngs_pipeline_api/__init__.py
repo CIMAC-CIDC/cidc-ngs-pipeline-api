e34292557f4b0c18dd5c1ab303757c55c6f67d1c
SC Van Nostrand <scvannost@gmail.com>
2020-10-14 18:07:56 -0400

    initial commit to wrap in minimal PyPI package

---
+++ cidc_ngs_pipeline_api/__init__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+import os
+from json import load
+
+
+__author__ = """Stephen C van Nostrand"""
+__email__ = "vannost@ds.dfci.harvard.edu"
+__version__ = "0.1.0"
+
+
+_API_ENDING = "_output_API.json"
+_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+_SCHEMA_PATH = os.path.join(_BASE_DIR, "output_API.schema.json")
+
+
+try:
+    with open(_SCHEMA_PATH, "r") as f:
+        METASCHEMA = load(f)
+except Exception as e:
+    raise Exception(f"Failed loading json {_SCHEMA_PATH}") from e
+
+SCHEMAS = {}
+for dname, _, files in os.walk(_BASE_DIR):
+    for fname in files:
+        if fname.endswith(_API_ENDING):
+            analysis = dname.split("/")[-1]
+            with open(os.path.join(dname, fname), "rb") as f:
+                SCHEMAS[analysis] = load(f)
---
+++ tests/test_apis.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests each output API against the output API schema"""
+
+import jsonschema
+import pytest
+
+from cidc_ngs_pipeline_api import METASCHEMA, SCHEMAS
+
+
+def test_schema():
+    """Ensure the output_API.schema.json conforms to JSON Schema Draft 7"""
+    jsonschema.Draft7Validator.check_schema(METASCHEMA)
+
+
+validator = jsonschema.Draft7Validator(METASCHEMA)
+
+
+@pytest.mark.parametrize("schema", [pytest.param(v, id=k) for k, v in SCHEMAS.items()])
+def test_api(schema):
+    """Ensure the output_API.json file conforms to schema"""
+    errors = list(validator.iter_errors(schema))  # Iterable[ValidationError]
+
+    # check that long_description isn't just short_description repeated
+    for k, v in schema.items():
+        for n, d in enumerate(v):
+            if not d["long_description"].replace(d["short_description"], "").strip():
+                errors.append(
+                    f"/{k}/{n} {d['file_path_template']} needs a long_description"
+                )
+
+    num_errors = len(errors)
+    if num_errors:
+        raise Exception(f"{num_errors} problems were found:" + "\n" + "\n".join(errors))
