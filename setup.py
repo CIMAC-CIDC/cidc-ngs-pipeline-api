d09c2ace976d5fa94a7d65a4553c4d3cdc46ae43
SC Van Nostrand <scvannost@gmail.com>
2020-10-19 12:07:40 -0700

    Wrapping in PyPI deploy-able module (#18)
    * initial commit to wrap in minimal PyPI package
    
    * Update README.md
    
    * switch from `pytest` to `python -m pytest`
    
    * updated output_API for wes/chips to satisfy descriptions content tests
    
    * skipping content checks for wes; reset wes_output_API.json to same as master
    
    * make black happy; run_id -> run id
    
    * remove pypi deploy
    
    * remove pytest-benchmark dependency
    
    * Create .gitignore, copied from cidc-api-gae repo

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+import os
+import glob
+from setuptools import setup, find_packages
+
+with open("README.md") as readme_file:
+    readme = readme_file.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+from cidc_ngs_pipeline_api import __author__, __email__, __version__
+
+setup(
+    author=__author__,
+    author_email=__email__,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    description="The CIDC NGS Pipeline output APIs",
+    python_requires=">=3.6",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme,
+    include_package_data=True,
+    keywords="cidc_ngs_pipeline_api",
+    name="cidc_ngs_pipeline_api",
+    packages=find_packages(include=["cidc_ngs_pipeline_api"]),
+    test_suite="tests",
+    url="https://github.com/CIMAC-CIDC/cidc-ngs-pipeline-api",
+    version=__version__,
+    zip_safe=False,
+)
---
+++ tests/test_apis.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests each output API against the output API schema"""
+
+import jsonschema
+import pytest
+
+from cidc_ngs_pipeline_api import METASCHEMA, SCHEMAS
+
+
+def test_schema():
+    """Ensure the output_API.schema.json conforms to JSON Schema Draft 7"""
+    jsonschema.Draft7Validator.check_schema(METASCHEMA)
+
+
+validator = jsonschema.Draft7Validator(METASCHEMA)
+
+
+@pytest.mark.parametrize("schema", [pytest.param(v, id=k) for k, v in SCHEMAS.items()])
+def test_api(schema):
+    """Ensure the output_API.json file conforms to schema"""
+    errors = list(validator.iter_errors(schema))  # Iterable[ValidationError]
+
+    # check that long_description isn't just short_description repeated
+    if "run id" not in schema:  # exclude wes
+        for k, v in schema.items():
+            for n, d in enumerate(v):
+                if (
+                    not d["long_description"]
+                    .replace(d["short_description"], "")
+                    .strip()
+                ):
+                    errors.append(
+                        f"/{k}/{n} {d['file_path_template']} needs a long_description"
+                    )
+
+    assert len(errors) == 0, "\n".join(errors)
